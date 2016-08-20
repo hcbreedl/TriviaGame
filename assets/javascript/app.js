518591c49774e8bbfcd59092a3337910a5588e4b
Houston Breedlove <wehaveaproblem16@Houstons-MacBook-Pro.local>
2016-08-19 20:26:07 -0400

    final commit

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,5 +1,5 @@
 $( document ).ready(function() {
-	console.log("ready!");
+
 
 var correctAnswers = 0;
 var incorrectAnswers = 0;
@@ -15,8 +15,8 @@ var a2 = $('<button id="button" type="button" class="btn btn-default btn-lg btn-
 var a3 = $('<button id="button" type="button" class="btn btn-default btn-lg btn-block"></button>');
 var a4 = $('<button id="button" type="button" class="btn btn-default btn-lg btn-block"></button>');
 
+// Timer Necessities ==========================
 number = 21;
-
 
 function run(){
     counter = setInterval(decrement, 1000);
@@ -50,14 +50,10 @@ function timerReset() {
 function stop(){
     clearInterval(counter);
 }
+//=============================================
 
-function resetColor() {
-	$(a1).css('color', 'black');
-	$(a2).css('color', 'black');
-	$(a3).css('color', 'black');
-	$(a4).css('color', 'black');
-}
-
+// Makes the questions and answers get out of the way of
+// showing whether or not user's choice was correct or incorrect
 function removeAll() {
 	a4.remove();
 	a3.remove();
@@ -66,14 +62,7 @@ function removeAll() {
 	q1.remove();
 }
 
-function fadeInChoices() {
-	$('#title').append(q1.hide().fadeIn(1000))
-	$('#title').append(a1.hide().fadeIn(2000));
-	$('#title').append(a2.hide().fadeIn(2500));
-	$('#title').append(a3.hide().fadeIn(3000));
-	$('#title').append(a4.hide().fadeIn(3500));
-}
-
+// Displaying correct or incorrect choice
 function correct() {
 		correctAnswers = correctAnswers + 1;
 		console.log("correct: " + correctAnswers);
@@ -105,18 +94,47 @@ function incorrect() {
 		badJob.fadeOut(1000);
 		};
 }
+//========================================
 
+function fadeInChoices() {
+	$('#title').append(q1.hide().fadeIn(1000))
+	$('#title').append(a1.hide().fadeIn(2000));
+	$('#title').append(a2.hide().fadeIn(2500));
+	$('#title').append(a3.hide().fadeIn(3000));
+	$('#title').append(a4.hide().fadeIn(3500));
+}
 
 // First questions load
 $('#startButton').on('click', function() {
 	question1();
 });
 
+
+//===================================================================
+/* For the following questions the following logic applies:
+
+	(question1 removes initial start button)
+
+	1. Update the html in q1, a1, a2, a3, a4.
+
+	2. Fade in the question and answers.
+
+	3. Have timer start to decrement.
+
+	4. First on click function is always the Correct choice 
+		The following three are always the incorrect options
+		a. On click functions stop the timer, remove all questions
+			and answers, runs correct function (informing the user
+			whether their choice was correct or not), then applying 
+			a timeout with the following question function to run. 
+
+	5. Question 10 includes a couple new functions in attempt to keep
+		code as DRY as possible.*/
+//====================================================================
+
 function question1 () {
 
 	$('#startButton').remove();
-
-	resetColor();
 
 	$(q1).html("Who were the founders of Apple Computer Inc.?");
 	$(a1).html("Jennifer Aniston and Monica Lewinski");
@@ -129,7 +147,7 @@ function question1 () {
 	decrement();
 	run();
 
-	// The correct answer turns green.
+	// Correct choice
 	$(a2).on('click', function() {	
 		stop();	
 		removeAll();
@@ -137,7 +155,7 @@ function question1 () {
 		q2 = setTimeout(question2, 2000);
 	});
 
-	//The incorrect answers turn red.
+	// Incorrect choices
 	$(a1).on('click', function() {	
 		stop();
 		removeAll();
@@ -161,8 +179,6 @@ function question1 () {
 function question2 () {
 	timerReset();
 
-	resetColor();
-
 	$(q1).html("How much was the first Apple computer?");
 	$(a1).html("$100");
 	$(a2).html("$1000");
@@ -171,7 +187,7 @@ function question2 () {
 
 	fadeInChoices()
 
-	// The correct answer turns green.
+	// Correct choice
 	$(a3).on('click', function() {
 		stop();
 		removeAll();
@@ -179,7 +195,7 @@ function question2 () {
 		q3 = setTimeout(question3, 2000);
 	});
 
-	//The incorrect answers turn red.
+	// Incorrect choices
 	$(a1).on('click', function() {	
 		stop();
 		removeAll();
@@ -203,8 +219,6 @@ function question2 () {
 function question3 () {
 	timerReset();
 
-	resetColor();
-
 	$(q1).html("Which Apple store was the most expensive to build?");
 	$(a1).html("The one in at SouthPoint");
 	$(a2).html("The one is Hawaii");
@@ -213,7 +227,7 @@ function question3 () {
 
 	fadeInChoices()
 
-	// The correct answer turns green.
+	// Correct choice
 	$(a4).on('click', function() {
 		stop();
 		removeAll();
@@ -221,7 +235,7 @@ function question3 () {
 		q4 = setTimeout(question4, 2000);
 	});
 
-	//The incorrect answers turn red.
+	// Incorrect choices
 	$(a1).on('click', function() {	
 		stop();
 		removeAll();
@@ -245,8 +259,6 @@ function question3 () {
 function question4 () {
 	timerReset();
 
-	resetColor();
-
 	$(q1).html("How many US states don't have an Apple store?");
 	$(a1).html("9");
 	$(a2).html("They all have one");
@@ -255,7 +267,7 @@ function question4 () {
 
 	fadeInChoices()
 
-	// The correct answer turns green.
+	// Correct choice
 	$(a1).on('click', function() {
 		stop();
 		removeAll();
@@ -263,7 +275,7 @@ function question4 () {
 		q5 = setTimeout(question5, 2000);
 	});
 
-	//The incorrect answers turn red.
+	// Incorrect choices
 	$(a4).on('click', function() {	
 		stop();
 		removeAll();
@@ -287,8 +299,6 @@ function question4 () {
 function question5 () {
 	timerReset();
 
-	resetColor();
-
 	$(q1).html("Why was the Lisa computer given its name?");
 	$(a1).html("It was named after Steve Jobs' former lover");
 	$(a2).html("It was named after Steve Jobs' daughter");
@@ -297,7 +307,7 @@ function question5 () {
 
 	fadeInChoices()
 
-	// The correct answer turns green.
+	// Correct choice
 	$(a2).on('click', function() {
 		stop();
 		removeAll();
@@ -305,34 +315,29 @@ function question5 () {
 		q6 = setTimeout(question6, 2000);
 	});
 
-	//The incorrect answers turn red.
+	// Incorrect choice
 	$(a4).on('click', function() {	
 		stop();
 		removeAll();
 		incorrect();
-		
 		q6 = setTimeout(question6, 2000);
 	});
 	$(a1).on('click', function() {
 		stop();
 		removeAll();
 		incorrect();
-		
 		q6 = setTimeout(question6, 2000);
 	});
 	$(a3).on('click', function() {
 		stop();
 		removeAll();
 		incorrect();
-		
 		q6 = setTimeout(question6, 2000);
 	});
 }
 
 function question6 () {
 	timerReset();
-
-	resetColor();
 
 	$(q1).html("What year was the original Ipod released?");
 	$(a1).html("1970");
@@ -342,43 +347,37 @@ function question6 () {
 
 	fadeInChoices()
 
-	// The correct answer turns green.
+	// Correct choice
 	$(a4).on('click', function() {
 		stop();
 		removeAll();
 		correct();
-		
 		q7 = setTimeout(question7, 2000);
 	});
 
-	//The incorrect answers turn red.
+	// Incorrect choices
 	$(a2).on('click', function() {	
 		stop();
 		removeAll();
 		incorrect();
-		
 		q7 = setTimeout(question7, 2000);
 	});
 	$(a1).on('click', function() {
 		stop();
 		removeAll();
 		incorrect();
-		
 		q7 = setTimeout(question7, 2000);
 	});
 	$(a3).on('click', function() {
 		stop();
 		removeAll();
 		incorrect();
-		
 		q7 = setTimeout(question7, 2000);
 	});
 }
 
 function question7 () {
 	timerReset();
-
-	resetColor();
 
 	$(q1).html("How many songs could the first Ipod hold?");
 	$(a1).html("27");
@@ -388,35 +387,31 @@ function question7 () {
 
 	fadeInChoices()
 
-	// The correct answer turns green.
+	// Correct choice
 	$(a3).on('click', function() {
 		stop();
 		removeAll();
 		correct();
-		
 		q8 = setTimeout(question8, 2000);
 	});
 
-	//The incorrect answers turn red.
+	// Incorrect choices
 	$(a2).on('click', function() {	
 		stop();
 		removeAll();
 		incorrect();
-		
 		q8 = setTimeout(question8, 2000);
 	});
 	$(a1).on('click', function() {
 		stop();
 		removeAll();
 		incorrect();
-		
 		q8 = setTimeout(question8, 2000);
 	});
 	$(a4).on('click', function() {
 		stop();
 		removeAll();
 		incorrect();
-		
 		q8 = setTimeout(question8, 2000);
 	});
 }
@@ -424,53 +419,45 @@ function question7 () {
 function question8 () {
 	timerReset();
 
-	resetColor();
-
 	$(q1).html("Before co-founding Apple, where did Steve Jobs work?");
 	$(a1).html("Atari");
-	$(a2).html("McDonals");
+	$(a2).html("McDonalds");
 	$(a3).html("IBM");
 	$(a4).html("Burger King");
 
 	fadeInChoices()
 
-	// The correct answer turns green.
+	// Correct choice
 	$(a1).on('click', function() {
 		stop();
 		removeAll();
 		correct();
-		
 		q9 = setTimeout(question9, 2000);
 	});
 
-	//The incorrect answers turn red.
+	// Incorrect choices
 	$(a2).on('click', function() {	
 		stop();
 		removeAll();
 		incorrect();
-		
 		q9 = setTimeout(question9, 2000);
 	});
 	$(a3).on('click', function() {
 		stop();
 		removeAll();
 		incorrect();
-		
 		q9 = setTimeout(question9, 2000);
 	});
 	$(a4).on('click', function() {
 		stop();
 		removeAll();
 		incorrect();
-		
 		q9 = setTimeout(question9, 2000);
 	});
 }
 
 function question9 () {
 	timerReset();
-
-	resetColor();
 
 	$(q1).html("What religious practices did Steve Jobs hold?");
 	$(a1).html("Christianity");
@@ -480,43 +467,60 @@ function question9 () {
 
 	fadeInChoices()
 
-	// The correct answer turns green.
+	// Correct choice
 	$(a2).on('click', function() {
 		stop();
 		removeAll();
 		correct();
-		
 		q10 = setTimeout(question10, 2000);
 	});
 
-	//The incorrect answers turn red.
+	// Incorrect choices
 	$(a1).on('click', function() {	
 		stop();
 		removeAll();
 		incorrect();
-		
 		q10 = setTimeout(question10, 2000);
 	});
 	$(a3).on('click', function() {
 		stop();
 		removeAll();
 		incorrect();
-		
 		q10 = setTimeout(question10, 2000);
 	});
 	$(a4).on('click', function() {
 		stop();
 		removeAll();
 		incorrect();
-		
 		q10 = setTimeout(question10, 2000);
 	});
 }
 
+// Start back at first question and update correct and incorrect answers.
+function startReturn () {			
+	$(startButton).remove();
+	$(youWin).remove();
+	number = 21;
+	$('#timer').html('<h2>00</h2>');
+	correctAnswers = 0;
+	incorrectAnswers = 0;
+
+	question1();
+}
+
+// What happens when the question 10 is answered.
+function q10Incorrect () {
+	incorrectAnswers++;
+	stop();
+	$('#timer').html('<h2>00</h2>');	
+	removeAll();	
+	$('#title').append(youWin);
+	$(youWin).html('<p>Thanks for playing!<br>Correct Answers: ' + correctAnswers + ' <br>Incorrect Answers: ' + incorrectAnswers + '<br>Play Again?</p>');
+	$('#title').append(startButton);
+}
+
 function question10 () {
 	timerReset();
-
-	resetColor();
 
 	$(q1).html("What was Apple's first slogan?");
 	$(a1).html("Apple is the Best");
@@ -528,10 +532,11 @@ function question10 () {
 
 	// Clicking the correct answer
 	$(a4).on('click', function() {
-		
+		correctAnswers++;
+
 		stop();
 		$('#timer').html('<h2>00</h2>');
-		correctAnswers++;
+		
 		removeAll();
 		
 		$('#title').append(youWin);
@@ -539,76 +544,30 @@ function question10 () {
 		$('#title').append(startButton);
 		
 		$('#startButton').on('click', function() {
-			$(startButton).remove();
-			$(youWin).remove();
-			number = 21;
-			$('#timer').html('<h2>00</h2>');
-			correctAnswers = 0;
-			incorrectAnswers = 0;
-			question1();
+			startReturn();
 		})
 	});
 
 	//The incorrect answers turn red.
 	$(a1).on('click', function() {	
-		stop();
-		$('#timer').html('<h2>00</h2>');
-		
-		removeAll();
-		incorrectAnswers++;
-		$('#title').append(youWin);
-		$(youWin).html('<p>Thanks for playing!<br>Correct Answers: ' + correctAnswers + ' <br>Incorrect Answers: ' + incorrectAnswers + '<br>Play Again?</p>');
-
-		$('#title').append(startButton);
+		q10Incorrect();
 		
 		$('#startButton').on('click', function() {
-			$(startButton).remove();
-			$(youWin).remove();
-			number = 21;
-			$('#timer').html('<h2>00</h2>');
-			correctAnswers = 0;
-			incorrectAnswers = 0;
-			question1();
+			startReturn();
 		})
 	});
 	$(a3).on('click', function() {
-		stop();
-		$('#timer').html('<h2>00</h2>');
-		
-		removeAll();
-		incorrectAnswers++;
-		$('#title').append(youWin);
-		$(youWin).html('<p>Thanks for playing!<br>Correct Answers: ' + correctAnswers + ' <br>Incorrect Answers: ' + incorrectAnswers + '<br>Play Again?</p>');
-		$('#title').append(startButton);
+		q10Incorrect();
 		
 		$('#startButton').on('click', function() {
-			$(startButton).remove();
-			$(youWin).remove();
-			number = 21;
-			$('#timer').html('<h2>00</h2>');
-			correctAnswers = 0;
-			incorrectAnswers = 0;
-			question1();
+			startReturn();
 		})
 	});
 	$(a2).on('click', function() {
-		stop();
-		$('#timer').html('<h2>00</h2>');
-		
-		removeAll();
-		incorrectAnswers++;
-		$('#title').append(youWin);
-		$(youWin).html('<p>Thanks for playing!<br>Correct Answers: ' + correctAnswers + ' <br>Incorrect Answers: ' + incorrectAnswers + '<br>Play Again?</p>');
-		$('#title').append(startButton);
+		q10Incorrect();
 		
 		$('#startButton').on('click', function() {
-			$(startButton).remove();
-			$(youWin).remove();
-			number = 21;
-			$('#timer').html('<h2>00</h2>');
-			correctAnswers = 0;
-			incorrectAnswers = 0;
-			question1();
+			startReturn();
 		})
 	});
 }
